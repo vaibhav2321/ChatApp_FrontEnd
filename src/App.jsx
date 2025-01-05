af841704c93c390d0b7ddd241ef64bfe2bf9bb10
Vaibhav2321 <vaibhavverma2321@gmail.com>
2025-01-05 21:33:16 +0530

    add folders {components, lib, pages, store}, loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {Routes, Route} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
+import { useAuthStore } from "./store/useAuthStore";
+import { useEffect } from "react";
+import {Loader} from "lucide-react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const {authUser, checkAuth, isCheckingAuth} = useAuthStore();
+  useEffect(()=>{
+    checkAuth()
+  },[checkAuth]);
 
+  if( isCheckingAuth & !authUser) return (
+    <div className="flex items-center justify-center h-screen">
+      <Loader className="size-10 animate-spin"/>
+    </div>
+  )
+  
   return (
     <>
-    <h1 className='underline'>hkdkdk</h1>
+    {authUser}
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<HomePage />}></Route>
+      <Route path="/signup" element={<SignUpPage />}></Route>
+      <Route path="/login" element={<LoginPage />}></Route>
+      <Route path="/settings" element={<SettingsPage />}></Route>
+      <Route path="/profile" element={<ProfilePage />}></Route>
+    </Routes>
     </>
   )
 }
